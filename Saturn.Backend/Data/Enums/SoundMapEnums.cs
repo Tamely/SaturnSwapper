9f05318306b2a8e9c0917a1358cf1ab4556799e0
Owen <47339578+owen-developer@users.noreply.github.com>
2022-01-17 12:45:16 -0600

    Pickaxe thingy sounds or whatever

---
+++ Saturn.Backend/Data/Enums/SoundMapEnums.cs
@@ -0,0 +1,15 @@
+﻿using CUE4Parse.UE4.Objects.UObject;
+using System.Collections.Generic;
+
+namespace Saturn.Backend.Data.Structures
+{
+    enum EFortReloadFXState
+    {
+        ReloadEnd
+    }
+    
+    public enum EFortWeaponSoundState
+    {
+        Normal
+    }
+}
