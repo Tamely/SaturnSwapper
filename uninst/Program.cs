af38355609e05214ce581d46ab914addbed0c660
Finite <remieluri@gmail.com>
2022-03-06 15:25:25 -0500

    Completely changed how the uninstaller works

--- uninst/Program.cs
+++ uninst/Program.cs
@@ -1,105 +1,115 @@
-﻿using System.Diagnostics;
-using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace Uninstaller;
 
 public static class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        Uninstall();
+        Uninstall().GetAwaiter().GetResult();
     }
 
-    internal static void Uninstall()
+    private static async Task Uninstall()
     {
-        try
-        {
-            string[] currentAssembly =
-            {
-                Assembly.GetExecutingAssembly().Location,
-                AppDomain.CurrentDomain.BaseDirectory + $"{typeof(Program).Assembly.GetName().Name}.exe"
-            };
+        var currentAssemblyPath = AppDomain.CurrentDomain.BaseDirectory;
+        var dataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Saturn\\";
 
-            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            var dataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Saturn\\";
-            var cd_AllFiles = GetAllFilesInDirectory(baseDirectory, currentAssembly);
-            foreach (var file in cd_AllFiles)
-            {
-                DeletePath(file, 1);
-            }
-            var cd_AllDirectories = GetAllSubDirectories(baseDirectory);
-            cd_AllDirectories.Reverse();
-            foreach (var directory in cd_AllDirectories)
-            {
-                DeletePath(directory, 0);
-            }
-            var data_AllFiles = GetAllFilesInDirectory(dataPath);
-            foreach (var file in data_AllFiles)
-            {
-                DeletePath(file, 1);
-            }
-            var data_AllDirectories = GetAllSubDirectories(dataPath);
-            data_AllDirectories.Reverse();
-            foreach (var directory in data_AllDirectories)
-            {
-                DeletePath(directory, 0);
-            }
-            foreach (var file in currentAssembly)
-            {
-                Process.Start("cmd.exe", $"/C choice /C Y /N /D Y /T 3 & Del {file}");
-            }
-        }
-        catch (Exception e)
+        var currentDirectctoryTree = await DirectoryTree.CreateDirectoryTreeAsync(currentAssemblyPath);
+        var plugins = currentDirectctoryTree.GetFiles(x => x.Path.Contains(".json") &&
+                                             x.FileContents.Contains("AssetPath")); // Gets all plugins. All plugin files should contain a part that says "Appdata"
+        foreach (var plugin in plugins)
         {
-            Console.WriteLine(e.ToString());
-            Console.ReadKey();
+            plugin.Delete();
+        }
+
+        var assemblyFileNames = new string[] { "uninst.exe", "uninst.dll" }; // Essentially the partial paths of the assembly.
+
+        var targetFiles = new string[] { "Saturn.exe", "oo2core_9_win64.dll", "uninst.r", "uninst.p", "uninst.deps" };
+        var targetDirectories = new string[] { "wwwroot", "Saturn.exe.WebView2" };
+
+        await DeleteDirectoryTree(currentDirectctoryTree, targetFiles, targetDirectories);
+
+        var adDirectoryTree = await DirectoryTree.CreateDirectoryTreeAsync(dataPath);
+        var adPlugins = adDirectoryTree.GetFiles(x => x.Path.Contains(".json") &&
+                                                 x.FileContents.Contains("AssetPath")); // Check if this directory contains any plugins
+
+        foreach (var plugin in adPlugins)
+        {
+            plugin.Delete();
+        }
+
+        await DeleteDirectoryTree(adDirectoryTree, null, null);
+        
+        var assemblyFiles = new List<SFile>();
+        foreach (var name in assemblyFileNames)
+        {
+            assemblyFiles.Add(currentDirectctoryTree.GetFile(name)); // Get the current assembly, and it's executing parent
+        }
+
+        // This command force deletes this assembly.
+        var command = "/C choice /C Y /N /D Y /T 3 & Del ";
+        foreach (var file in assemblyFiles)
+        {
+            Process.Start("cmd.exe", command + file.Path);
         }
     }
 
-    internal static List<string> GetAllSubDirectories(string directory)
+    private static async Task DeleteDirectoryTree(DirectoryTree tree, string[] partialFilePaths, string[] partialDirectoryPaths)
     {
-        var subDirectories = new List<string>();
-        var level = Directory.GetDirectories(directory).ToList();
-        subDirectories.AddRange(level);
-        foreach (var dir in level)
+        if (!Directory.Exists(tree.BaseDirectory.Path))
         {
-            subDirectories.AddRange(GetAllSubDirectories(dir));
+            return;
         }
 
-        return subDirectories;
-    }
-
-    internal static List<string> GetAllFilesInDirectory(string directory, string[] exclude = null)
-    {
-        var files = new List<string>();
-        files.AddRange(Directory.GetFiles(directory));
-        if (exclude != null)
+        var files = tree.GetFiles(partialFilePaths, 1).ToList();
+        if (partialDirectoryPaths is not null)
         {
-            foreach (var file in exclude)
+            foreach (var directory in partialDirectoryPaths)
             {
-                files.Remove(file);
+                var path = tree.GetNestedDirectory(directory).Path;
+                if (!Directory.Exists(path))
+                {
+                    continue;
+                }
+                else
+                {
+                    var dTree = await DirectoryTree.CreateDirectoryTreeAsync(tree.GetNestedDirectory(directory).Path);
+                    files.AddRange(dTree.GetFiles());
+                }
             }
         }
 
-        var subDirs = GetAllSubDirectories(directory);
-        foreach (var dir in subDirs)
+        foreach (var file in files)
         {
-            files.AddRange(GetAllFilesInDirectory(dir, null));
+            file.Delete();
         }
 
-        return files;
-    }
-
-    internal static void DeletePath(string path, int type)
-    {
-        if (type == 0 &&
-            Directory.Exists(path))
+        if (partialDirectoryPaths is not null)
         {
-            Directory.Delete(path);
+            foreach (var directory in partialDirectoryPaths)
+            {
+                var path = tree.GetNestedDirectory(directory).Path;
+                if (!Directory.Exists(path))
+                {
+                    continue;
+                }
+                else
+                {
+                    tree.GetNestedDirectory(directory).Delete();
+                }
+            }
         }
-        else if (type == 1 &&
-                 File.Exists(path))
+        else
         {
-            File.Delete(path);
+            foreach (var child in tree.BaseDirectory.Children)
+            {
+                child.Delete();
+            }
         }
     }
 }
---
+++ uninst/SBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Uninstaller;
+
+internal abstract class SBase
+{
+    public abstract void Delete();
+
+    public abstract string Path { get; }
+}
