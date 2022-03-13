240c5d64d8077ff4dc69ab5558f51b653cfccc0f
Finite <remieluri@gmail.com>
2022-03-13 15:54:54 -0400

    Added comments

--- uninst/Program.cs
+++ uninst/Program.cs
@@ -10,51 +10,73 @@ namespace Uninstaller;
 
 public static class Program
 {
+    // The uninitialized path for the log file, in case the uninstaller breaks.
     private static string _logPath;
 
     public static void Main()
     {
+        // C# doesn't like async main methods (At least not console applications) so we just do this
         Uninstall().GetAwaiter().GetResult();
     }
 
+    // Uninstalls everything related to Saturn, including plugins, and all data.
     private static async Task Uninstall()
     {
+        // Try just in case we run into an error
         try
         {
+            // Gets the current directory of the running app
             var currentAssemblyPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Initialize log path
             _logPath = $"{currentAssemblyPath}\\uninst.log";
+
+            // Get path for Saturn's data folder
             var dataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Saturn";
 
+            // Initialize a new DirectoryTree with 'dataPath'
             var dataTree = await DirectoryTree.CreateDirectoryTreeAsync(dataPath);
+
+            // Delete the data folder
             await DeleteDirectoryTree(dataTree);
 
+            // Initialize a new DirectoryTree with the assembly's current directory
             var currentTree = await DirectoryTree.CreateDirectoryTreeAsync(currentAssemblyPath);
+
+            // Gets all the plugins in the current path
             var plugins = GetPlugins(currentTree);
+
+            // Delete plugins
             foreach (var plugin in plugins)
             {
                 plugin.Delete();
             }
 
+            // Create an array of the partial paths of the folders associated with Saturn
             var targetFolder = new string[]
             {
                 "wwwroot",
                 "Saturn.exe.WebView2"
             };
 
+            // Create an array of the partial paths of the files associated Saturn
             var targetFiles = new string[]
             {
                 "oo2core_9_win64.dll",
                 "Saturn"
             };
 
+            // Delete the target folders
             foreach (var item in targetFolder)
             {
                 var tree = await DirectoryTree.CreateDirectoryTreeAsync($"{currentAssemblyPath}\\{item}");
                 await DeleteDirectoryTree(tree);
             }
 
-            // This command force deletes this assembly.
+            // This command force deletes any application
             var command = "/C choice /C Y /N /D Y /T 3 & Del ";
+
+            // Delete all target files
             foreach (var file in targetFiles)
             {
                 var path = currentTree.GetFile(file).Path;
