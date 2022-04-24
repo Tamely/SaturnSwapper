6e1e491334221d604755d931d8f2bded330e26f1
Josh <64652542+Tamely@users.noreply.github.com>
2022-04-23 20:19:42 -0500

    Update the model for swaps to work with the IDs

--- CUE4Parse/CUE4Parse/SaturnData.cs
+++ CUE4Parse/CUE4Parse/SaturnData.cs
@@ -1,4 +1,4 @@
-﻿namespace CUE4Parse;
+namespace CUE4Parse;
 
 public class SaturnData
 {
@@ -6,6 +6,22 @@ public class SaturnData
     public static byte[] CompressedData { get; set; }
     public static long Offset { get; set; }
     public static string Path { get; set; }
-    public static int Parition { get; set; } = 0;
     public static string UAssetPath { get; set; }
+    public static ZLIBBlock? Block { get; set; } = null;
+    public static string SearchCID { get; set; } = "NOCID";
+}
+
+public class ZLIBBlock
+{
+    public long Start { get; set; }
+    public long End { get; set; }
+    public byte[] DecompressedData { get; set; }
+    public byte[] CompressedData { get; set; }
+    public ZLIBBlock(long start, long end, byte[] decompressedData, byte[] compressedData)
+    {
+        Start = start;
+        End = end;
+        DecompressedData = decompressedData;
+        CompressedData = compressedData;
+    }
 }
