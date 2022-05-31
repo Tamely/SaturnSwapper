edc3d62c6d8b7028880f56541b4ba6fa2e9c4bf9
Fruthy <91793946+Fruthy-Ue@users.noreply.github.com>
2022-05-31 10:48:09 +0200

    Add files via upload

---
+++ Saturn.Backend/Data/SwapOptions/Pickaxes/TheFretBasherSwap.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using Saturn.Backend.Data.Enums;
+using Saturn.Backend.Data.Models.Items;
+
+namespace Saturn.Backend.Data.SwapOptions.Pickaxes;
+
+internal sealed class TheFretBasherSwap : PickaxeSwap
+{
+    public TheFretBasherSwap(string name, string rarity, string icon, Dictionary<string, string> swaps, EFortRarity rarityEnum)
+        : base(name, rarity, icon, swaps, rarityEnum)
+    {
+    }
+
+    public override List<SaturnAsset> Assets =>
+        new()
+        {
+            new SaturnAsset()
+            {
+                ParentAsset = "FortniteGame/Content/Athena/Items/Weapons/WID_Harvest_Pickaxe_BelieverFemale",
+                Swaps = new List<SaturnSwap>()
+                {
+                    new SaturnSwap()
+                    {
+                        Search = System.Convert.ToBase64String(new byte[] { 252, 255, 255, 255 }),
+                        Replace = System.Convert.ToBase64String(new byte[] { 0, 0, 0, 0 }),
+                        Type = SwapType.Property
+                    },
+                    new SaturnSwap()
+                    {
+                        Search = System.Convert.ToBase64String(new byte[] { 255, 255, 255, 3 }),
+                        Replace = System.Convert.ToBase64String(new byte[] { 255, 255, 255, (byte)RarityEnum }),
+                        Type = SwapType.Property
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/UI/Foundation/Textures/Icons/Weapons/Items/T-Icon-Pickaxes-BelieverStickerPickaxe.T-Icon-Pickaxes-BelieverStickerPickaxe",
+                        Replace = Swaps["SmallIcon"],
+                        Type = SwapType.SmallIcon
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/UI/Foundation/Textures/Icons/Weapons/Items/T-Icon-Pickaxes-BelieverStickerPickaxe-L.T-Icon-Pickaxes-BelieverStickerPickaxe-L",
+                        Replace = "/",
+                        Type = SwapType.LargeIcon
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/Weapons/FORT_Melee/Pickaxe_Believer_Stickers_Female/Meshes/Believer_Stickers_Female_Axe.Believer_Stickers_Female_Axe",
+                        Replace = Swaps["Mesh"],
+                        Type = SwapType.WeaponMesh
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/Athena/Sounds/Weapons/PickAxes/BelieverStickersFemale/PickaxeSwing_BelieverStickersFemale.PickaxeSwing_BelieverStickersFemale",
+                        Replace = Swaps["SwingCue"],
+                        Type = SwapType.WeaponSound
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/Athena/Sounds/Weapons/PickAxes/BelieverStickersFemale/PickaxeReady_BelieverStickersFemale.PickaxeReady_BelieverStickersFemale",
+                        Replace = Swaps["EquipCue"],
+                        Type = SwapType.WeaponSound
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/Athena/Sounds/Weapons/PickAxes/BelieverStickersFemale/PickaxeImpactEnemy_BelieverStickersFemale.PickaxeImpactEnemy_BelieverStickersFemale",
+                        Replace = Swaps["ImpactCue"],
+                        Type = SwapType.WeaponSound
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/Weapons/FORT_Melee/Pickaxe_Believer_Stickers_Female/FX/NS_Pickaxe_Believer_Stickers_AnimTrail.NS_Pickaxe_Believer_Stickers_AnimTrail",
+                        Replace = Swaps["Trail"],
+                        Type = SwapType.WeaponTrail
+                    },
+                    new SaturnSwap()
+                    {
+                        Search = "/Game/Weapons/FORT_Melee/Pickaxe_Believer_Stickers_Female/FX/NS_Pickaxe_Believer_Stickers_Impact.NS_Pickaxe_Believer_Stickers_Impact",
+                        Replace = Swaps["ImpactFX"],
+                        Type = SwapType.WeaponFx
+                    }
+                }
+            }
+        };
+}
