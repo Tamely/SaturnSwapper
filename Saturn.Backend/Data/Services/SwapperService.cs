d0ca4c8649ef748c45de14b3519a589fc6c184ea
ect8zy <106690677+ect8zy@users.noreply.github.com>
2022-06-03 14:36:44 +0300

    Added Arctic Assassin (SwapperServices.cs)

--- Saturn.Backend/Data/Services/SwapperService.cs
+++ Saturn.Backend/Data/Services/SwapperService.cs
@@ -1376,6 +1376,10 @@ public sealed class SwapperService : ISwapperService
                                                                            item.Rarity.BackendValue,
                                                                            item.Images.SmallIcon,
                                                                            option.SwapModel).ToSaturnOption(),
+            "CID_037_Athena_Commando_F_WinterCamo" => new ArcicAssassinSkinSwap(item.Name,
+                                                                                item.Rarity.BackendValue,
+                                                                                item.Images.SmallIcon,
+                                                                                option.SwapModel).ToSaturnOption(),
             _ => new SaturnOption()
         };
     }
