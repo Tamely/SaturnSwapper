09d4491f735d54016f902eb6304712c8fa28b784
ect8zy <106690677+ect8zy@users.noreply.github.com>
2022-06-03 14:57:10 +0300

    added Cape Of Potassium (BackblingSwap.cs)

---
+++ Saturn.Backend/Data/SwapOptions/Backblings/CapeOfPotassium.cs
@@ -0,0 +1,39 @@
+﻿using Saturn.Backend.Data.Enums;
+using Saturn.Backend.Data.Models.Items;
+using System.Collections.Generic;
+
+namespace Saturn.Backend.Data.SwapOptions.Backblings;
+
+internal sealed class CapeOfPotassiumBackblingSwap : BackblingSwap
+{
+    public CapeOfPotassiumBackblingSwap(string name, string rarity, string icon, Dictionary<string, string> data)
+        : base(name, rarity, icon, data)
+    {
+    }
+
+    public override List<SaturnAsset> Assets =>
+        new()
+        {
+            new SaturnAsset()
+            {
+                ParentAsset = "FortniteGame/Content/Characters/CharacterParts/Backpacks/CP_Backpack_BananaLeader",
+                Swaps = new List<SaturnSwap>()
+                {
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/Accessories/FORT_Capes/M_MED_Banana_Leader/Meshes/M_MED_Banana_Leader_Pack.M_MED_Banana_Leader_Pack",
+                        Replace = Data["Mesh"],
+                        Type = SwapType.BackblingMesh
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/Accessories/FORT_Capes/M_MED_Banana_Leader/Meshes/M_MED_Banana_Leader_Pack_AnimBP.M_MED_Banana_Leader_Pack_AnimBP_C",
+                        Replace = Data["ABP"] ?? "/Game/Accessories/FORT_Capes/M_MED_Banana_Leader/Meshes/M_MED_Banana_Leader_Pack_AnimBP.M_MED_Banana_Leader_Pack_AnimBP_C",
+                        Type = SwapType.BackblingAnim
+                    }
+                }
+            }
+        };
+}
