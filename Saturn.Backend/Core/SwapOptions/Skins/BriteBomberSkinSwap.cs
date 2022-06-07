94c86c18ed0da4f3e9013124ebf1f23d26c2e3b1
ect8zy <106690677+ect8zy@users.noreply.github.com>
2022-06-08 00:23:13 +0300

    brite bomber

---
+++ Saturn.Backend/Core/SwapOptions/Skins/BriteBomberSkinSwap.cs
@@ -0,0 +1,126 @@
+﻿using Saturn.Backend.Core.Enums;
+using Saturn.Backend.Core.Models.Items;
+using System.Collections.Generic;
+
+namespace Saturn.Backend.Core.SwapOptions.Skins;
+
+internal sealed class BriteBomberSkinSwap : SkinSwap
+{
+    public BriteBomberSkinSwap(string name, string rarity, string icon, MeshDefaultModel swapModel)
+        : base(name, rarity, icon, swapModel)
+    {
+    }
+
+    public override List<SaturnAsset> Assets =>
+        new()
+        {
+            new SaturnAsset()
+            {
+                ParentAsset =
+                    "FortniteGame/Content/Athena/Heroes/Meshes/Bodies/CP_Commando_SciPop_F",
+                Swaps = new()
+                {
+                    new()
+                    {
+                        Search =
+                            "/Game/Characters/Player/Female/Medium/Bodies/F_Med_Soldier_01/Meshes/F_Med_Soldier_01",
+                        Replace = SwapModel.BodyMesh,
+                        Type = SwapType.BodyMesh
+                    },
+                    new()
+                    {
+                        Search =
+                            "/Game/Characters/Player/Female/Medium/Bodies/F_Med_Soldier_01/Meshes/F_Med_Soldier_01_Skeleton_AnimBP.F_Med_Soldier_01_Skeleton_AnimBP_C",
+                        Replace = SwapModel.BodyABP ??
+                                  "/Game/Characters/Player/Female/Medium/Bodies/F_Med_Soldier_01/Meshes/F_Med_Soldier_01_Skeleton_AnimBP.F_Med_Soldier_01_Skeleton_AnimBP_C",
+                        Type = SwapType.BodyAnim
+                    },
+                    new()
+                    {
+                        Search =
+                            "/Game/Characters/Player/Female/Medium/Bodies/F_Med_Soldier_01/Skins/BR_SciPop/Materials/F_MED_Commando_Body_SciPop.F_MED_Commando_Body_SciPop",
+                        Replace = SwapModel.BodyMaterials[0],
+                        Type = SwapType.BodyMaterial
+                    },
+                    new()
+                    {
+                        Search =
+                            "/Game/Characters/Player/Female/Medium/Base/SK_M_Female_Base_Skeleton.SK_M_Female_Base_Skeleton",
+                        Replace = SwapModel.BodySkeleton,
+                        Type = SwapType.BodySkeleton
+                    },
+                }
+            },
+            new SaturnAsset()
+            {
+                ParentAsset =
+                    "FortniteGame/Content/Athena/Heroes/Meshes/Heads/F_Med_His_Ramirez_Head_SciPop_ATH",
+                Swaps = new()
+                {
+                    new SaturnSwap()
+                    {
+                        Search = "/Game/Characters/CharacterColorSwatches/Hair/HairColor_01.HairColor_01",
+                        Replace = SwapModel.HeadHairColor,
+                        Type = SwapType.HairColor
+                    },
+                    new SaturnSwap()
+                    {
+                        Search = "/Game/Characters/CharacterColorSwatches/Hair/HairColor_01.HairColor_01",
+                        Replace = SwapModel.HeadSkinColor,
+                        Type = SwapType.SkinTone
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/Characters/Player/Female/Medium/Heads/F_MED_HIS_Ramirez_Head_01/Mesh/F_MED_HIS_Ramirez_Head_01_AnimBP_Child.F_MED_HIS_Ramirez_Head_01_AnimBP_Child_C",
+                        Replace = SwapModel.HeadABP ??
+                                  "/Game/Characters/Player/Female/Medium/Heads/F_MED_HIS_Ramirez_Head_01/Mesh/F_MED_HIS_Ramirez_Head_01_AnimBP_Child.F_MED_HIS_Ramirez_Head_01_AnimBP_Child_C",
+                        Type = SwapType.HeadAnim
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/Characters/Player/Female/Medium/Heads/F_MED_HIS_Ramirez_Head_01/Mesh/F_MED_HIS_Ramirez_Head_01.F_MED_HIS_Ramirez_Head_01",
+                        Replace = SwapModel.HeadMesh,
+                        Type = SwapType.HeadMesh
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/Characters/Player/Female/Medium/Heads/F_Med_Head_01/Materials/SciPop/F_MED_Commando_Hair_SciPop.F_MED_Commando_Hair_SciPop",
+                        Replace = SwapModel.HeadMaterials[0],
+                        Type = SwapType.HeadMaterial
+                    },
+                    new SaturnSwap()
+                    {
+                        Search =
+                            "/Game/Characters/Player/Female/Medium/Heads/F_Med_Head_01/Materials/SciPop/F_MED_HIS_Ramirez_SciPop.F_MED_HIS_Ramirez_SciPop",
+                        Replace = SwapModel.HeadMaterials[1],
+                        Type = SwapType.HairMaterial
+                    },
+                }
+            },
+            new SaturnAsset()
+            {
+                ParentAsset =
+                    "FortniteGame/Content/Characters/CharacterParts/Hats/Ramirez_Glasses_SciPop",
+                Swaps = new()
+                {
+                    new()
+                    {
+                        Search =
+                            "/Game/Accessories/Hats/Mesh/Ramirez_Glasses.Ramirez_Glasses",
+                        Replace = SwapModel.FaceACCMesh,
+                        Type = SwapType.FaceAccessoryMesh
+                    },
+                    new()
+                    {
+                        Search =
+                            "/Game/Characters/Player/Female/Medium/Bodies/F_MED_Commando_01/Materials/F_MED_Commando_Glasses_01_SciPop.F_MED_Commando_Glasses_01_SciPop",
+                        Replace = SwapModel.FaceACCMaterials[0],
+                        Type = SwapType.FaceAccessoryMaterial
+                    }
+                }
+            }
+        };
+}
