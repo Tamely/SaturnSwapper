bff005e521440f0ac7960b8dbe823c5a6e5cf0bb
Stern <90482851+SternI@users.noreply.github.com>
2022-07-16 21:10:42 +0200

    Skin icons

--- Saturn.Backend/Core/Utils/Constants.cs
+++ Saturn.Backend/Core/Utils/Constants.cs
@@ -1,11 +1,11 @@
-﻿namespace Saturn.Backend.Core.Utils
+namespace Saturn.Backend.Core.Utils
 {
     public class Constants
     {
         public const string CidPath = "FortniteGame/Content/Athena/Items/Cosmetics/Characters/";
 
         public const string EidPath = "FortniteGame/Content/Athena/Items/Cosmetics/Dances/";
-        
+
         public const string BidPath = "FortniteGame/Content/Athena/Items/Cosmetics/Backpacks/";
         public const string NewBidPath = "FortniteGame/Content/Athena/Items/Cosmetics/BackPacks/";
 
@@ -20,6 +20,24 @@
 
         public const string PidPath = "FortniteGame/Content/Athena/Items/Cosmetics/Pickaxes/";
         public const string NewPidPath = "FortniteGame/Content/Athena/Items/Cosmetics/PickAxes/";
+
+        public static string[] DefaultIcons = new string[]
+        {
+            "T-Soldier-HID-A-272-Athena-Commando-F-Prime",
+            "T-Soldier-HID-A-273-Athena-Commando-F-Prime-B",
+            "T-Soldier-HID-A-274-Athena-Commando-F-Prime-C",
+            "T-Soldier-HID-A-275-Athena-Commando-F-Prime-D",
+            "T-Soldier-HID-A-276-Athena-Commando-F-Prime-E",
+            "T-Soldier-HID-A-277-Athena-Commando-F-Prime-F",
+            "T-Soldier-HID-A-278-Athena-Commando-F-Prime-G",
+            "T-Soldier-HID-A-279-Athena-Commando-M-Prime",
+            "T-Soldier-HID-A-280-Athena-Commando-M-Prime-B",
+            "T-Soldier-HID-A-281-Athena-Commando-M-Prime-C",
+            "T-Soldier-HID-A-282-Athena-Commando-M-Prime-D",
+            "T-Soldier-HID-A-283-Athena-Commando-M-Prime-E",
+            "T-Soldier-HID-A-284-Athena-Commando-M-Prime-F",
+            "T-Soldier-HID-A-285-Athena-Commando-M-Prime-G"
+        };
 
         public const string UserVersion = "1.4.10";
     }
