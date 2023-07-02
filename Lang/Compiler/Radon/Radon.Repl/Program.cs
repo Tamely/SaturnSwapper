460ae024b91c93952da9d3abaf4923947cc3365e
Finite <remieluri@gmail.com>
2023-07-02 11:49:11 -0400

    Fixed a race condition issue

--- Lang/Compiler/Radon/Radon.Repl/Program.cs
+++ Lang/Compiler/Radon/Radon.Repl/Program.cs
@@ -10,75 +10,70 @@ namespace Radon.Repl;
 
 public static class Program
 {
-    private static readonly object _lock = new();
-    
     public static void Main()
     {
-        lock (_lock)
+        while (true)
         {
-            while (true)
+            var text = Console.ReadLine();
+            if (text == null)
             {
-                var text = Console.ReadLine();
-                if (text == null)
-                {
-                    break;
-                }
+                break;
+            }
+            
+            if (!File.Exists(text))
+            {
+                Console.WriteLine($"File '{text}' does not exist.");
+                continue;
+            }
+            
+            Log("Reading source file...", ConsoleColor.Cyan);
+            var sourceText = SourceText.From(File.ReadAllText(text), text);
+            Log("Parsing source file...", ConsoleColor.Cyan);
+            var syntaxTree = SyntaxTree.Parse(sourceText);
+            Log("Generating compilation...", ConsoleColor.Cyan);
+            var compilation = new Compilation(syntaxTree);
+            var diagnostics = compilation.Diagnostics;
+            if (diagnostics.Any())
+            {
+                Log("Diagnostics were found...", ConsoleColor.Red);
+                Console.WriteLine();
+                Console.Out.WriteDiagnostics(diagnostics);
+            }
+            else
+            {
+                Log("No diagnostics were found...", ConsoleColor.Green);
+                var root = syntaxTree.Root;
                 
-                if (!File.Exists(text))
-                {
-                    Console.WriteLine($"File '{text}' does not exist.");
-                    continue;
-                }
+#if DEBUG
+                root.WriteTo(Console.Out);
                 
-                Log("Reading source file...", ConsoleColor.Cyan);
-                var sourceText = SourceText.From(File.ReadAllText(text), text);
-                Log("Parsing source file...", ConsoleColor.Cyan);
-                var syntaxTree = SyntaxTree.Parse(sourceText);
-                Log("Generating compilation...", ConsoleColor.Cyan);
-                var compilation = new Compilation(syntaxTree);
-                var diagnostics = compilation.Diagnostics;
-                if (diagnostics.Any())
+                var included = syntaxTree.Included;
+                foreach (var include in included)
                 {
-                    Log("Diagnostics were found...", ConsoleColor.Red);
                     Console.WriteLine();
-                    Console.Out.WriteDiagnostics(diagnostics);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    include.Root.WriteTo(Console.Out);
                 }
-                else
-                {
-                    Log("No diagnostics were found...", ConsoleColor.Green);
-                    var root = syntaxTree.Root;
-                    
-    #if DEBUG
-                    root.WriteTo(Console.Out);
-                    
-                    var included = syntaxTree.Included;
-                    foreach (var include in included)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        include.Root.WriteTo(Console.Out);
-                    }
-    #endif
-    #if PARSE_ONLY
-                    continue;
-    #endif
+#endif
+#if PARSE_ONLY
+                continue;
+#endif
 
-                    Log("Compiling...", ConsoleColor.Cyan);
-                    var bytes = compilation.Compile(out diagnostics);
-                    if (bytes == null)
-                    {
-                        Log("Compilation failed...", ConsoleColor.Red);
-                        Console.Out.WriteDiagnostics(diagnostics);
-                        continue;
-                    }
-                    
-                    Log("Compilation succeeded...", ConsoleColor.Green);
-                    Log($"Writing to {sourceText.FileName}.csp...", ConsoleColor.Cyan);
-                    File.WriteAllBytes(sourceText.FileName + ".csp", bytes);
-                    Log("Done!", ConsoleColor.Green);
-                    
+                Log("Compiling...", ConsoleColor.Cyan);
+                var bytes = compilation.Compile(out diagnostics);
+                if (bytes == null)
+                {
+                    Log("Compilation failed...", ConsoleColor.Red);
+                    Console.Out.WriteDiagnostics(diagnostics);
+                    continue;
                 }
+                
+                Log("Compilation succeeded...", ConsoleColor.Green);
+                Log($"Writing to {sourceText.FileName}.csp...", ConsoleColor.Cyan);
+                File.WriteAllBytes(sourceText.FileName + ".csp", bytes);
+                Log("Done!", ConsoleColor.Green);
+                
             }
         }
     }
