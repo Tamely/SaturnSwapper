6dd5b93efe23e354ee9eff9dc919f6aa7affc8c7
Finite <remieluri@gmail.com>
2023-08-20 19:53:15 -0400

    IL Viewer

---
+++ Lang/Compiler/Radon/ILViewer/ILToken.cs
@@ -0,0 +1,14 @@
+﻿namespace ILViewer;
+
+// ReSharper disable once InconsistentNaming
+internal sealed class ILToken
+{
+    public ILTokenKind Kind { get; }
+    public string Text { get; }
+    
+    public ILToken(ILTokenKind kind, string text)
+    {
+        Kind = kind;
+        Text = text;
+    }
+}
---
+++ Lang/Compiler/Radon/ILViewer/WriteLevel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ILViewer;
+
+internal enum WriteLevel
+{
+    Info = ConsoleColor.Cyan,
+    Warning = ConsoleColor.Yellow,
+    Error = ConsoleColor.Red
+}
