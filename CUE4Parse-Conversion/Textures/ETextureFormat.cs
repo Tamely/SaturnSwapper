b466bf046caf0854ab0ef3d1109a1178dfe2d4e7
Josh <joshua.audia@gmail.com>
2023-08-28 19:07:54 -0500

    CUE4Parse itself

---
+++ CUE4Parse-Conversion/Textures/ETextureFormat.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace CUE4Parse_Conversion.Textures
+{
+    public enum ETextureFormat
+    {
+        [Description("PNG")]
+        Png,
+        [Description("TGA (Not Implemented)")]
+        Tga,
+        [Description("DDS (Not Implemented)")]
+        Dds
+    }
+}
