75459aa6bc293c49b706ce11aa0e5e01c033845f
Fin <karstrbarnhardt@gmail.com>
2023-09-23 21:57:44 -0400

    Fixed issue with locals

--- Lang/Compiler/Radon/Radon/CodeAnalysis/Emit/Binary/MetadataBinary/Local.cs
+++ Lang/Compiler/Radon/Radon/CodeAnalysis/Emit/Binary/MetadataBinary/Local.cs
@@ -5,12 +5,14 @@ namespace Radon.CodeAnalysis.Emit.Binary.MetadataBinary;
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
 public readonly struct Local
 {
+    public readonly int ParentMethod;
     public readonly int Ordinal;
     public readonly int Name;
     public readonly int Type;
 
-    public Local(int ordinal, int name, int type)
+    public Local(int parentMethod, int ordinal, int name, int type)
     {
+        ParentMethod = parentMethod;
         Ordinal = ordinal;
         Name = name;
         Type = type;
